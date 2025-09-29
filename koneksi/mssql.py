d338e56280fb7fc611fb2d676f655a98a3c20b58
Johanes Gregorius Indra Pradana Pao <johanes@sosco.id>
2025-09-29 11:48:58 +0700

    Rename def eksekusi_query menjadi eksekusi_kueri

--- koneksi/mssql.py
+++ koneksi/mssql.py
@@ -1,34 +1,24 @@
 from mssql_python import Connection, connect
 from pandas import DataFrame
 
+
 def buka_koneksi(
-    server: str,
-    port: str,
-    database: str,
-    uid: str,
-    pwd: str
+    server: str, port: str, database: str, uid: str, pwd: str
 ) -> Connection:
     string_koneksi = (
-        f"SERVER={server},{port};"
-        f"DATABASE={database};"
-        f"UID={uid};"
-        f"PWD={pwd};"
-        f"Encrypt=no"
+        f"SERVER={server},{port};DATABASE={database};UID={uid};PWD={pwd};Encrypt=no"
     )
     print(string_koneksi)
     return connect(string_koneksi)
 
-def eksekusi_query(
-    koneksi: Connection,
-    query: str
-) -> DataFrame:
+
+def eksekusi_kueri(koneksi: Connection, query: str) -> DataFrame:
     kursor = koneksi.cursor()
     kursor.execute(query)
     hasil = kursor.fetchall()
     kolom = [kol[0] for kol in kursor.description]
     return DataFrame(hasil, columns=kolom)
 
-def tutup_koneksi(
-    koneksi: Connection
-) -> None:
+
+def tutup_koneksi(koneksi: Connection) -> None:
     koneksi.close()
