623a068c1829d3057160ea92f1b9f26e902810c0
Johanes Gregorius Indra Pradana Pao <johanes@sosco.id>
2025-09-29 17:53:40 +0700

    main.py generate csv
    Commit ini menambahkan fungsi - fungsi utama ke dalam skrip main.py
    sampai dengan generate laporan csv.
    
    Pengiriman file ke SFTP dan log dalam email belum dilakukan.

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import logging
 from utilitas.cli_parse import parse_argumen
+from utilitas.eval_argumen import eval_argumen
 from utilitas.rahasia import Rahasia
+from utilitas.laporan import generate
 
-argumen = parse_argumen()
-rahasia = Rahasia()
+FILE_LOG = "log_aplikasi.log"
+
+# Buat root logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Handler untuk menulis ke FILE_LOG
+handler_file = logging.FileHandler(FILE_LOG, mode="w")
+handler_file.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(message)s"))
+logger.addHandler(handler_file)
+
+# Handler untuk menulis ke console
+handler_konsol = logging.StreamHandler()
+handler_konsol.setFormatter(
+    logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+)
+logger.addHandler(handler_konsol)
+
+if __name__ == "__main__":
+    argumen = parse_argumen()
+    mode_skrip = eval_argumen(argumen)
+    rahasia = Rahasia()
+    generate(mode_skrip, rahasia.db)
