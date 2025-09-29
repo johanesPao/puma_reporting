18af4a92efc1fcadac6b580506dcb7412ba45e65
Johanes Gregorius Indra Pradana Pao <johanes@sosco.id>
2025-09-29 12:11:04 +0700

    Merubah self.db dan self.sftp dari dict menjadi dataclass

--- utilitas/rahasia.py
+++ utilitas/rahasia.py
@@ -1,20 +1,40 @@
+from dataclasses import dataclass
 import os
 from dotenv import load_dotenv
+
+
+@dataclass
+class KredensialDatabase:
+    server: str
+    port: str
+    database: str
+    uid: str
+    pwd: str
+
+
+@dataclass
+class KredensialSFTP:
+    host: str
+    port: str
+    protocol: str
+    user: str
+    password: str
+
 
 class Rahasia:
     def __init__(self):
         load_dotenv()
-        self.db = {
-            "server": os.getenv("DB_SERVER"),
-            "port": os.getenv("DB_PORT"),
-            "database": os.getenv("DB_DATABASE"),
-            "uid": os.getenv("DB_UID"),
-            "pwd": os.getenv("DB_PWD")
-        }
-        self.sftp = {
-            "host": os.getenv("SFTP_HOST"),
-            "port": os.getenv("SFTP_PORT"),
-            "protocol": os.getenv("SFTP_PROTOCOL"),
-            "user": os.getenv("SFTP_USER"),
-            "pass": os.getenv("SFTP_PASS")
-        }
+        self.db = KredensialDatabase(
+            server=os.getenv("DB_SERVER"),
+            port=os.getenv("DB_PORT"),
+            database=os.getenv("DB_DATABASE"),
+            uid=os.getenv("DB_UID"),
+            pwd=os.getenv("DB_PWD"),
+        )
+        self.sftp = KredensialSFTP(
+            host=os.getenv("SFTP_HOST"),
+            port=os.getenv("SFTP_PORT"),
+            protocol=os.getenv("SFTP_PROTOCOL"),
+            user=os.getenv("SFTP_USER"),
+            password=os.getenv("SFTP_PASSWORD"),
+        )
