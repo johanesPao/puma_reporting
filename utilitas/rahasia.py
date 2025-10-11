34730b3e8aaf236440eff140d55e27712a4e6d34
Johanes Gregorius Indra Pradana Pao <johanes@sosco.id>
2025-10-12 02:07:59 +0700

    Konstan kredensial rahasia saat build time dan perubahan flow GitHub Action

--- utilitas/rahasia.py
+++ utilitas/rahasia.py
@@ -2,6 +2,32 @@ from utilitas.logging import log_dan_waktu
 from dataclasses import dataclass
 import os
 from dotenv import load_dotenv
+from infisical_sdk import InfisicalSDKClient
+import logging
+
+try:
+    # Konstan ini hanya eksis saat proses build
+    # type: ignore[import]
+    from rahasia_buildinfo import (
+        ENV_HOST,
+        ENV_PROJECT_ID,
+        ENV_CLIENT_ID,
+        ENV_CLIENT_SECRET,
+    )
+
+    ENV_MODE = None
+except ImportError:
+    # fallback ke lokal development
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    ENV_MODE = os.getenv("ENV_MODE")
+    ENV_HOST = os.getenv("ENV_HOST")
+    ENV_PROJECT_ID = os.getenv("ENV_PROJECT_ID")
+    ENV_CLIENT_ID = os.getenv("ENV_CLIENT_ID")
+    ENV_CLIENT_SECRET = os.getenv("ENV_CLIENT_SECRET")
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -30,27 +56,48 @@ class KredensialEmail:
     bcc: str
 
 
-@log_dan_waktu("Membaca dan memuat rahasia lingkungan")
+@log_dan_waktu("Membaca dan memuat rahasia")
 class Rahasia:
     def __init__(self):
         load_dotenv()
-        self.db = KredensialDatabase(
-            server=os.getenv("DB_SERVER"),
-            port=os.getenv("DB_PORT"),
-            database=os.getenv("DB_DATABASE"),
-            uid=os.getenv("DB_UID"),
-            pwd=os.getenv("DB_PWD"),
-        )
-        self.sftp = KredensialSFTP(
-            host=os.getenv("SFTP_HOST"),
-            port=os.getenv("SFTP_PORT"),
-            user=os.getenv("SFTP_USER"),
-            password=os.getenv("SFTP_PASSWORD"),
-        )
-        self.email = KredensialEmail(
-            username=os.getenv("GMAIL_ACCOUNT"),
-            password=os.getenv("GMAIL_APP_PASSWORD"),
-            to=os.getenv("TO_ADDRESS"),
-            cc=os.getenv("CC_ADDRESS"),
-            bcc=os.getenv("BCC_ADDRESS"),
-        )
+
+        try:
+            logger.info(f"🔗 Menghubungkan ke {ENV_HOST}...")
+            klien_env = InfisicalSDKClient(ENV_HOST)
+
+            klien_env.auth.universal_auth.login(ENV_CLIENT_ID, ENV_CLIENT_SECRET)
+
+            list_rahasia = klien_env.secrets.list_secrets(
+                project_id=ENV_PROJECT_ID,
+                environment_slug="dev" if ENV_MODE == "dev" else "produksi",
+                secret_path="/",
+                expand_secret_references=True,
+            ).secrets
+
+            logger.info("📦 Unpack rahasia ke dalam proses env...")
+            if list_rahasia:
+                for rahasia in list_rahasia:
+                    os.environ[rahasia.secretKey] = rahasia.secretValue
+
+            self.db = KredensialDatabase(
+                server=os.getenv("DB_SERVER"),
+                port=os.getenv("DB_PORT"),
+                database=os.getenv("DB_DATABASE"),
+                uid=os.getenv("DB_UID"),
+                pwd=os.getenv("DB_PWD"),
+            )
+            self.sftp = KredensialSFTP(
+                host=os.getenv("SFTP_HOST"),
+                port=os.getenv("SFTP_PORT"),
+                user=os.getenv("SFTP_USER"),
+                password=os.getenv("SFTP_PASSWORD"),
+            )
+            self.email = KredensialEmail(
+                username=os.getenv("GMAIL_ACCOUNT"),
+                password=os.getenv("GMAIL_APP_PASSWORD"),
+                to=os.getenv("TO_ADDRESS"),
+                cc=os.getenv("CC_ADDRESS"),
+                bcc=os.getenv("BCC_ADDRESS"),
+            )
+        except Exception as e:
+            logger.error(f"⚠️ Gagal membaca dan memuat rahasia: {e}")
---
+++ utilitas/rahasia_buildinfo.template.py
@@ -0,0 +1,7 @@
+# File ini akan di-generate ulang pada saat build time.
+# DO NOT EDIT!!
+
+ENV_HOST = "__ENV_HOST__"
+ENV_PROJECT_ID = "__ENV_PROJECT_ID__"
+ENV_CLIENT_ID = "__ENV_CLIENT_ID__"
+ENV_CLIENT_SECRET = "__ENV_CLIENT_SECRET__"
